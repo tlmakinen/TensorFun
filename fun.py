23bc54dcabfbb47d7dc58b56a47ed82de831d055
T Lucas Makinen <tmakinen@princeton.edu>
2018-09-24 00:32:50 +0100

    loaded data, going to look into theory next

---
+++ fun.py
@@ -0,0 +1,20 @@
+# First TensorFlow program!
+# Tutorial from https://www.datacamp.com/community/tutorials/tensorflow-tutorial?utm_source=adwords_ppc
+
+import tensorflow as tf
+import numpy as np
+# Initialize Constants
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+# Multiply
+result = tf.multiply(x1, x2)
+ 
+# Initialize the TensorFlow session
+sess = tf.Session()
+
+# Print result to display
+print(sess.run(result), "\n", "You did it!")
+
+# close the session
+sess.close()
